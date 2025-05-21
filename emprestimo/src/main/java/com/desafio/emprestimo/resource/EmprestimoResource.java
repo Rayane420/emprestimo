0338a747d3e7b14dabeabc1c0cb575dc670f89e5
Rayane Maciel <46855211+Rayane420@users.noreply.github.com>
2025-05-21 11:39:09 -0300

    Adicionando validação de campos do cliente

--- emprestimo/src/main/java/com/desafio/emprestimo/resource/EmprestimoResource.java
+++ emprestimo/src/main/java/com/desafio/emprestimo/resource/EmprestimoResource.java
@@ -3,6 +3,7 @@ package com.desafio.emprestimo.resource;
 import com.desafio.emprestimo.resource.dto.ClienteRequestEmprestimo;
 import com.desafio.emprestimo.resource.dto.ClienteResponseEmprestimo;
 import com.desafio.emprestimo.service.EmprestimoService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +20,7 @@ public class EmprestimoResource {
     }
 
     @PostMapping("/customer-loans")
-    public ResponseEntity<ClienteResponseEmprestimo> customerLoans(@RequestBody ClienteRequestEmprestimo clienteRequest){
+    public ResponseEntity<ClienteResponseEmprestimo> customerLoans(@Valid @RequestBody ClienteRequestEmprestimo clienteRequest){
         var emprestimoResponse = emprestimoService.verificarEmprestimosDisponiveis(clienteRequest);
         return ResponseEntity.ok(emprestimoResponse);
     }
--- emprestimo/src/main/java/com/desafio/emprestimo/resource/dto/ClienteRequestEmprestimo.java
+++ emprestimo/src/main/java/com/desafio/emprestimo/resource/dto/ClienteRequestEmprestimo.java
@@ -1,12 +1,16 @@
 package com.desafio.emprestimo.resource.dto;
 
 import com.desafio.emprestimo.domain.Cliente;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
 
-public record ClienteRequestEmprestimo(Integer idade,
-                                       String cpf,
-                                       String nome,
-                                       Double salario,
-                                       String localizacao) {
+public record ClienteRequestEmprestimo(@Min(value = 18) Integer idade,
+                                       @CPF String cpf,
+                                       @NotBlank String nome,
+                                       @NotNull Double salario,
+                                       @NotBlank String localizacao) {
 
     public Cliente toCliente() {
         return new Cliente(
